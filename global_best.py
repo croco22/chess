acf4975bfa33bc86bd793e9c97f58109a017fc51
philipplandeck <philipp.landeck@t-online.de>
2025-05-03 10:18:16 +0200

    Recommendations working

--- global_best.py
+++ global_best.py
@@ -8,7 +8,7 @@ board = chess.Board(sys.argv[1])
 engine_path = "stockfish/stockfish-windows-x86-64-avx2.exe"
 engine = chess.engine.SimpleEngine.popen_uci(engine_path)
 
-result = engine.play(board, chess.engine.Limit(time=5.0))
+result = engine.play(board, chess.engine.Limit(time=0.5))
 engine.quit()
 
 print(result.move)

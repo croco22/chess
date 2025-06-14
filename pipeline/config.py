d1f907b111b1b0632af2bdac680c55e3e40160ff
philipplandeck <philipp.landeck@t-online.de>
2025-06-14 11:31:10 +0200

    Adjust parameters in config file

--- pipeline/config.py
+++ pipeline/config.py
@@ -4,7 +4,7 @@ FEATURE_LABELS = {
     "fragility_score": "Fragility Score",
     "variance": "Variance"
 }
-ELOS = list(range(800, 2201, 100))
-ELO_DEV = 50
-MIN_SAMPLES_PER_GROUP = 25
+ELO_DEV = 100
+ELOS = list(range(1000, 1900, ELO_DEV))
+MIN_SAMPLES_PER_GROUP = 15
 WINRATE_THRESHOLD = 0.03

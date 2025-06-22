69d3fd4bada0b12a88e2fcf9d29fb132ba426d93
philipplandeck <philipp.landeck@t-online.de>
2025-06-22 15:59:20 +0200

    Adjust matplotlib font

--- pipeline/config.py
+++ pipeline/config.py
@@ -1,3 +1,17 @@
+import os
+
+import matplotlib.font_manager as fm
+import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+
+load_dotenv()
+
+font_path = os.getenv("PT_SANS_FONT")
+if font_path and os.path.isfile(font_path):
+    fm.fontManager.addfont(font_path)
+    plt.rcParams["font.family"] = fm.FontProperties(fname=font_path).get_name()
+    plt.rcParams["pdf.fonttype"] = 42  # Embed font as TrueType
+
 FEATURES = ["delta", "fragility_score", "variance"]
 FEATURE_LABELS = {
     "delta": "Rating Delta",
@@ -6,5 +20,5 @@ FEATURE_LABELS = {
 }
 ELO_DEV = 100
 ELOS = list(range(1000, 1900, ELO_DEV))
-MIN_SAMPLES_PER_GROUP = 15
-WINRATE_THRESHOLD = 0.03
+MIN_SAMPLES_PER_GROUP = 10
+WINRATE_THRESHOLD = 0.025
